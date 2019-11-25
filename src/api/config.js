321e4319a92cd7b7522e83b594e777f6e0bb2e45
lianjianqiang <632920053@qq.com>
2019-11-25 17:29:15 +0800

    temp: 临时

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,6 @@
 export default {
     // baseUrl: 'http://txh.logimis.com'
+    // baseUrl: 'http://192.168.0.106:8099'
     baseUrl: '/api'
     // baseUrl: process.env.NODE_ENV === 'production' ? 'http://txh.logimis.com' : '/api'
 }
